39fd979b25a0a05c78f1ed4dd700d89024a4f422
happysiro <happy.siro@gmail.com>
2020-05-12 17:52:33 +0900

    add alacritty config

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 mkdir ~/.config/
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/fish ~/.config/fish
+ln -s ~/dotfiles/alacritty ~/.config/alacritty
 
 cp -i ~/dotfiles/_gitconfig.local.example ~/.gitconfig.local
