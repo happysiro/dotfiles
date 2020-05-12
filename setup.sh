d39ee33be52a55fca0cf8557e745285eb21057c2
happysiro <happy.siro@gmail.com>
2020-05-12 17:44:20 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ ln -s ~/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/dotfiles/_gitignore ~/.gitignore
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 mkdir ~/.config/
-ln -s ~/dotfiles/nvim ~/nvim
-ln -s ~/dotfiles/fish ~/nvim
+ln -s ~/dotfiles/nvim ~/config/nvim
+ln -s ~/dotfiles/fish ~/config/fish
 
 cp -i ~/dotfiles/_gitconfig.local.example ~/.gitconfig.local
